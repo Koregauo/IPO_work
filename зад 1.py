a450eb810bb3a8eb37f7903457cff0d722548306
Koregauo <artemzilinskij02@gmail.com>
2023-10-11 09:49:18 +0300

    лаб 7 зад 1-2 ИПО

---
+++ зад 1.py
@@ -0,0 +1,7 @@
+a=('алуфа 8', 'кальево 684', 'дорофьево 3', 'августина 15', 'рало 59', 'советская 1', 'ярово 87')
+new_tu=a[0:3]
+print(new_tu)
+new_tu2=a[3:]
+print(new_tu2)
+all_tu=(new_tu+new_tu2)
+print(all_tu)
